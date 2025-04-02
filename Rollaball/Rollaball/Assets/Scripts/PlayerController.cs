547cac915790d7834cafbd4ff28278d9e481afbb
Camilo Prieto <prietocamilo21@gmail.com>
2025-04-02 11:35:10 -0500

    IN DEV >>> Se agregó el contador de puntos (Con errores)
    Por el momento el contador de puntos del jugador aun no funciona correctamente.

--- Rollaball/Rollaball/Assets/Scripts/PlayerController.cs
+++ Rollaball/Rollaball/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
+    public TextMeshProUGUI countText;
+
     private Rigidbody rb;
+    private int count;
     private float movementX;
     private float movementY;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
     }
         
     void OnMove(InputValue movementValue)
@@ -22,10 +29,25 @@ public class PlayerController : MonoBehaviour
 
     }//Funciones (Funtion Body) C#
 
+    void SetCountText() 
+   {
+       countText.text =  "Count: " + count.ToString();
+   }
+
     void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
         rb.AddForce(movement * speed);
     }
+
+    private void OnTriggerEnter (Collider other) 
+   {
+       if (other.gameObject.CompareTag("PickUp")) 
+       {
+           other.gameObject.SetActive(false);
+           count = count + 1;
+           SetCountText();
+       }
+   }
 }
